10a486277c34a782fe9fa8630c2243b3e83238ef
Xclipse26 <85234653+Xclipse26@users.noreply.github.com>
2022-12-14 09:28:35 +0630

    Znwl

--- app/Models/MAdmin.php
+++ app/Models/MAdmin.php
@@ -4,17 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class MAdmin extends Model
 {
     use HasFactory;
-
     public function updateAdminRole($ad_id, $roleId)
     {
         DB::table('m_admin')->where('id', $ad_id)->update([
             'role' => $roleId
 
         ]);
+    }
+
+    public function Addadmin()
+    {
+        DB:table('m_admin')
     }
 }
