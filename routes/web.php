8f2cc221b0e3e38e4709e0d8cb5a8529a7cc6e98
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2022-12-31 10:55:58 +0630

    some changes header

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,26 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
+// Route::get('/addInstructor', function () {
+//     return inertia("addInstructor");
+// })->name("addInstructor.view");
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 
-Route::get('/', function () {
+Route::get('/login', function () {
+    return inertia("Admin/AdLogin");
+});
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+// Route::middleware([checkRole::class])->group(function(){
+Route::get('/home', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -43,6 +60,7 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
 Route::get('/classview/{id?}', [viewclassController::class, "getclassdata"])->name("class.view");
 // Route::get('/classsorting/{name?}', [viewclassController::class, "classsorting"])->name("class.sorting");
 Route::get('/classscategory/{id?}', [viewclassController::class, "classcategory"])->name("class.category");
@@ -69,117 +87,27 @@ Route::post('/setting/upload_public', [SettingController::class, 'upload_public'
 Route::get('/setting', [SettingController::class, 'index'])->name("setting.index");
 
 
-Route::resource('admin' , AdminController::class);
-Route::get('/login',function(){
-    return inertia("Admin/AdLogin");
-});
-Route::post('/login',[LoginController::class,'store'])->name('login.store');
+// Route::resource('admin', AdminController::class);
 // Route::get('/addadmin',function(){
 //     return inertia("AddAdmin");
 // });
 // Route::get('/editadmin',function(){
 //     return inertia("EditAdmin");
 // });
+
 // Start Tools
-// Route::get('/mailTool', function () {
-//     return inertia("MailTool");
-// });
-// Route::get('/addguide',function(){
-//     return inertia("Addguide");
-// });
 Route::resource('mailTool', MailToolController::class);
 Route::resource('privacypolicyTool', PrivacyPolicyController::class);
 Route::resource('categoryTool', CategoryToolController::class);
 Route::resource('guideTool', GuideToolController::class);
 Route::resource('blogTool', BlogToolController::class);
-
 // End Tools
 
 // Start Admin Permission
-Route::resource('adminPermission',AdminPermissionController::class);
-
+Route::resource('adminPermission', AdminPermissionController::class);
 Route::resource('addRole', AddRoleController::class);
-
-Route::resource('addPage',AddPageController::class);
+Route::resource('pageList', AddPageController::class);
 // End Admin Permission
-
-=======
->>>>>>> origin/main
-// Route::get('/addInstructor', function () {
-//     return inertia("addInstructor");
-// })->name("addInstructor.view");
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
-
-Route::get('/login', function () {
-    return inertia("Admin/AdLogin");
-});
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
-
-// Route::middleware([checkRole::class])->group(function(){
-    Route::get('/home', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    });
-    
-    Route::get('/classview/{id?}', [viewclassController::class, "getclassdata"])->name("class.view")->middleware('checkRole');
-    Route::get('/classsorting/{name?}', [viewclassController::class, "classsorting"])->name("class.sorting")->middleware('checkRole');
-    Route::get('/classscategory/{id?}', [viewclassController::class, "classcategory"])->name("class.category")->middleware('checkRole');
-    Route::get('/addvideo', function () {
-        return Inertia::render('AddVideo');
-    });
-    
-    Route::resource('/class', ClassController::class);
-    
-    
-    Route::resource('/students', StudentController::class);
-    
-    
-    Route::resource('/instructors', InstructorController::class);
-    Route::get('/instructor/{class?}{name?}', [Instructor::class, 'index'])->name("instructor.index");
-    Route::get('/instructors/edit/{id}', [InstructorController::class, 'show'])->name("instructors.show");
-    Route::get('/instructors/create', [InstructorController::class, 'create'])->name("instructors.create");
-    
-    Route::post('/setting/upload', [SettingController::class, 'upload'])->name("setting.upload");
-    Route::post('/setting/upload_public', [SettingController::class, 'upload_public'])->name("setting.upload_public");
-    // Route::get('/setting', function () {
-    //     return inertia("SettingAdmin");
-    // });
-    Route::get('/setting', [SettingController::class, 'index'])->name("setting.index");
-    
-    
-    // Route::resource('admin', AdminController::class);
-    // Route::get('/addadmin',function(){
-    //     return inertia("AddAdmin");
-    // });
-    // Route::get('/editadmin',function(){
-    //     return inertia("EditAdmin");
-    // });
-    
-    // Start Tools
-    Route::resource('mailTool', MailToolController::class);
-    Route::resource('privacypolicyTool', PrivacyPolicyController::class);
-    Route::resource('categoryTool', CategoryToolController::class);
-    Route::resource('guideTool', GuideToolController::class);
-    Route::resource('blogTool', BlogToolController::class);
-    // End Tools
-    
-    // Start Admin Permission
-    Route::resource('adminPermission', AdminPermissionController::class);
-    Route::resource('addRole', AddRoleController::class);
-    Route::resource('pageList', AddPageController::class);
-    // End Admin Permission
 // });
 
 Route::resource('admin', AdminController::class);
