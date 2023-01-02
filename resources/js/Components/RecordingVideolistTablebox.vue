48d8b8d586ccabdd588223963da99b9f04be9001
Xclipse26 <85234653+Xclipse26@users.noreply.github.com>
2023-01-02 17:51:41 +0630

    znwl video upload

--- resources/js/Components/RecordingVideolistTablebox.vue
+++ resources/js/Components/RecordingVideolistTablebox.vue
@@ -1,6 +1,11 @@
 <script setup>
 
 import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
+const porps = defineProps({
+    
+    classdata: Number,
+});
+console.log(porps.classdata);
 </script>
 <template>
  <div class="custombackgroundcolor h-48   rounded-lg  mt-3 px-3 py-4 overflow-y-scroll">
@@ -54,12 +59,12 @@ import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
   </table>
 
   </div>
-  <Link :href="route('addvideo.show', 1)">
+  <a :href="route('addvideo.show', classdata)">
 <button class="float-right pt-0.5 custommb sm:w-2/6 w-24 h-7 mt-5  text-white rounded-lg flex justify-center bg-blue-600 hover:bg-blue-700 active:bg-blue-900 cusmargin">
 <img src="../../../public/img/bxs_video-plus.png" alt="" class="w-5 h-5 pt-0.5">
 <span class="ml-2 sm:text-base text-xs sm:pt-0 pt-1">Add Video</span>
 </button>
-</Link>
+</a>
 </template>
 
 <script>
